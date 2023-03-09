d6a064ef34ccedca097af137f3f43bce792ab49a
Camilo-neck <ccuello@unal.edu.co>
2023-03-09 18:35:52 -0500

    Install data handling libraries - React Redux - React hook forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
-    ripple: theme => ({
-        colors: theme('colors'),
-        modifierTransition: 'background 0.2s',
-        activeTransition: 'background 0.1s'
-    }),
   },
-  plugins: [
-    require('tailwindcss-ripple')(),
-  ],
+  plugins: [],
 }
