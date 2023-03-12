9759d6d7c6466e91a4c4d3f732745f06ab728f5b
Camilo-neck <ccuello@unal.edu.co>
2023-03-12 15:26:45 -0500

    Change index design a bit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,11 +50,6 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <p>{user.name}</p>
 
           {!user.name ? (
             <>
@@ -74,109 +69,41 @@ export default function Home() {
               </Link>
             </>
           ) :
-            <Button
-              variant='outlined'
-              onClick={() => dispatch(logoutUser())}
-              className='text-error-30 hover:bg-error-30/5 focus:bg-error-30/10 rounded-full border focus:border-neutral-30 hover:border-error-20 border-neutral-30'>
-              Logout
-            </Button>
+            <>
+              <p>
+                Nombre: <span className={styles.code}>{user.name}</span>
+              </p>
+              <p>
+                Email: <span className={styles.code}>{user.email}</span>
+              </p>
+              <Button
+                variant='outlined'
+                onClick={() => dispatch(logoutUser())}
+                className='text-error-30 hover:bg-error-30/5 focus:bg-error-30/10 rounded-full border focus:border-neutral-30 hover:border-error-20 border-neutral-30'>
+                Logout
+              </Button>
+            </>
           }
           <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
           </div>
         </div>
 
         <div className={styles.center}>
           <Image
-            className={styles.logo}
-            src="/next.svg"
+            src="/auth-coliving-people.png"
             alt="Next.js Logo"
-            width={180}
-            height={37}
+            width={420}
+            height={100}
             priority
           />
           <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
+            <p className='text-2xl font-bold font-mono'>RE</p>
           </div>
         </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+        <div className={styles.center}>
+          <p className='text-xl font-semibold font-mono'>
+            Lo mejor para convivir con tus Roomies y llevar una vida más sana y feliz.
+          </p>
         </div>
       </main>
     </>
